304ee1fe026ef4cf4ee310feb84db3d36c6a41ba
Alexander M <contactimedici@gmail.com>
2021-07-13 16:13:00 -0400

    alex update

--- macroc/settings.py
+++ macroc/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
       'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'macroc',
-            'USER': '',       # windows/linux-only
-            'PASSWORD': '', # windows/linux-only
+            'USER': 'postgres',       # windows/linux-only
+            'PASSWORD': 'Hermes770', # windows/linux-only
       }
 }
 
