a2f85c8a3b4a07a351bf7eacec5b6fbaf5da1832
domenic dinh <domenicdinh@domenics-MacBook-Pro.local>
2021-07-13 16:14:35 -0400

    updating

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/',views.about, name='about' ),
-    path('meals/', views.index,  name='index'),
-    path('meals/create/', views.Macro_create.as_view(), name='macrocreate'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup', views.signup, name='signup'),
+      path('', views.home, name='home'),
+      path('about/',views.about, name='about' ),
+      path('meals/', views.index,  name='index'),
+      path('meals/create/', views.Macro_create.as_view(), name='macrocreate'),
+      path('accounts/', include('django.contrib.auth.urls')),
+      path('accounts/signup', views.signup, name='signup'),
 ]
