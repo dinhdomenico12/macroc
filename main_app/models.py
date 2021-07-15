eabb50de53cf4fac762e82371534635b784f989c
domenic dinh <domenicdinh@domenics-MacBook-Pro.local>
2021-07-15 19:16:21 -0400

    FINAL CODE

--- main_app/models.py
+++ main_app/models.py
@@ -1,23 +1,18 @@
 from django.db import models
-from django.urls import reverse
 from django.contrib.auth.models import User
-from datetime import date
-
 class Goals(models.Model):
       calories= models.IntegerField()
       protein=models.IntegerField()
       carbohydrates= models.IntegerField()
       fats= models.IntegerField()
       user = models.ForeignKey(User, on_delete=models.CASCADE)
-
 class Macros(models.Model):
       name= models.CharField(max_length=100)
       calories= models.IntegerField()
       protein=models.IntegerField()
       carbohydrates= models.IntegerField()
       fats= models.IntegerField()
-      goals = models.ForeignKey(Goals, on_delete=models.CASCADE)
-      
+      user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -2,66 +2,76 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from main_app.models import Goals, Macros
 from django.views.generic.edit import CreateView, UpdateView
 # Create your views here.
+@login_required
 def index(request):
-      macros = Macros.objects.all()
+      macros = Macros.objects.filter(user=request.user)
       goals = Goals.objects.filter(user=request.user)
-      count = Macros.objects.values('calories', 'protein','carbohydrates','fats')
+      count = Macros.objects.values('calories', 'protein','carbohydrates','fats').filter(user=request.user)
       total = [
-        {"calories":0},
-        {"protein":0},
-        {"carbohydrates":0},
-        {"fats":0},]
+            {"calories":0},
+            {"protein":0},
+            {"carbohydrates":0},
+            {"fats":0},]
       calories = 0
       protein = 0
       carbohydrates = 0
       fats = 0
       for c in count:
-        calories += c['calories']
-        protein += c['protein']
-        carbohydrates += c['carbohydrates']
-        fats += c['fats']
+            calories += c['calories']
+            protein += c['protein']
+            carbohydrates += c['carbohydrates']
+            fats += c['fats']
       total[0] =  calories
       total[1] =  protein
       total[2] =  carbohydrates
       total[3] =  fats
       print(total[0])
       return render(request, 'macro/index.html', {'macros': macros,'goals':goals,'total':total})
+@login_required
 def home(request):
   return render(request, 'home.html')
-class Macro_create(CreateView):
+class Macro_create(LoginRequiredMixin,CreateView):
       model = Macros
-      fields = '__all__'
+      fields = ['name','calories', 'protein', 'carbohydrates', 'fats']
       success_url = '/meals/'
-class Goals_create(CreateView):
+      def form_valid(self, form):
+            form.instance.user = self.request.user
+            return super().form_valid(form)
+class Goals_create(LoginRequiredMixin,CreateView):
       model = Goals
-      fields = '__all__'
+      fields = ['calories', 'protein', 'carbohydrates', 'fats']
       success_url = '/meals/'
-class Macroupdate(UpdateView):
-    model = Macros
-    fields = ['name', 'calories','protein','carbohydrates','fats']
-    success_url = '/meals/'
+      def form_valid(self, form):
+            form.instance.user = self.request.user
+            return super().form_valid(form)
+class Macroupdate(LoginRequiredMixin,UpdateView):
+      model = Macros
+      fields = ['name', 'calories','protein','carbohydrates','fats']
+      success_url = '/meals/'
+@login_required
 def macro_delete(request, macro_id):
-    Macros.objects.filter(id=macro_id).delete()
-    return redirect('/meals/')
+      Macros.objects.filter(id=macro_id).delete()
+      return redirect('/meals/')
 # sign up form
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
+      error_message = ''
+      if request.method == 'POST':
+            # This is how to create a 'user' form object
+            # that includes the data from the browser
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
       # This will add the user to the database
-      user = form.save()
+                  user = form.save()
       # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+                  login(request, user)
+                  return redirect('index')
+      else:
+                  error_message = 'Invalid sign up - try again'
+      # A bad POST or a GET request, so render signup.html with an empty form
+      form = UserCreationForm()
+      context = {'form': form, 'error_message': error_message}
+      return render(request, 'registration/signup.html', context)
